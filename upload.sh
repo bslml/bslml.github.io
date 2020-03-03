706e81ea2c4b5069fecc89951047730cb4aa5c09
git <1533302779@qq.com>
2020-03-03 23:35:26 +0800

    update

--- upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-git add .
-git commit -m "update"
-git push -f https://bslml:gt147369lss@github.com/bslml/bslml.github.io.git
-git push -f https://bslml:co147369lss@git.coding.net/bslml/bslml.coding.me.git
